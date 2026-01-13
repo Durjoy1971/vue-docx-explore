47730d24c6542b59a6f056892c5890f3bf7039e3
Durjoy-BS23 <durjoy.barua@brainstation-23.com>
2026-01-13 20:05:46 +0600

    feat: migrate event handling functionality to a new component

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,6 @@ function say(message: string) {
 
 </script>
 
-<template>
-  <div class="bg-yellow-200 flex flex-col justify-center h-screen p-4">
-    <button
-      class="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-      @click="count++"
-    >
-      Increment Count
-    </button>
-    <button
-      class="bg-red-500 text-white px-4 py-2 rounded mb-4"
-      @click="reduceByTwo"
-    >
-      Decrement Count by 2
-    </button>
-    <p> Count is : {{ count }}</p>
-    <button @click="say('hello')" class="bg-green-500 text-white px-4 py-2 rounded my-2">Say hello</button>
-    <button @click="say('bye')" class="bg-red-500 text-white px-4 py-2 rounded my-2">Say bye</button>
-  </div>
-</template>
+
 
 <style scoped></style>
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const count = ref(0)
+
+function reduceByTwo(event: Event) {
+  count.value = count.value - 2;
+}
+
+function say(message: string) {
+  alert(message);
+}
+
+</script>
+
+<template>
+  <div class="bg-yellow-200 flex flex-col justify-center h-screen p-4">
+    <button
+      class="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+      @click="count++"
+    >
+      Increment Count
+    </button>
+    <button
+      class="bg-red-500 text-white px-4 py-2 rounded mb-4"
+      @click="reduceByTwo"
+    >
+      Decrement Count by 2
+    </button>
+    <p> Count is : {{ count }}</p>
+    <button @click="say('hello')" class="bg-green-500 text-white px-4 py-2 rounded my-2">Say hello</button>
+    <button @click="say('bye')" class="bg-red-500 text-white px-4 py-2 rounded my-2">Say bye</button>
+  </div>
+</template>
+
+<style scoped></style>
